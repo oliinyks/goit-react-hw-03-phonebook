4f1e7ae31ce831973d7b88f258f08bdca2421ca0
Olijnyks <101010916+oliinyks@users.noreply.github.com>
2022-09-30 11:29:11 +0300

    add prop

---
+++ src/components/App/App.jsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { nanoid } from 'nanoid';
+import Notiflix from 'notiflix';
+
+import Form from 'components/Form';
+import ContactList from 'components/ContactList';
+import Filter from 'components/Filter';
+
+import css from './App.module.css';
+
+class App extends React.Component {
+  state = {
+    contacts: [],
+    filter: '',
+    visible: false,
+  };
+
+  formSubmitHandler = ({ name, number }) => {
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    const filter = this.state.contacts.filter(
+      ({ name }) => name === newContact.name
+    );
+
+    if (filter.length) {
+      Notiflix.Notify.failure('You already have a contact with that name');
+      return;
+    }
+
+    this.setState(({ contacts }) => ({
+      contacts: [...contacts, newContact],
+      visible: true,
+    }));
+    Notiflix.Notify.success('You have just created a new contact');
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+    }));
+    Notiflix.Notify.success('You have just deleted a contact');
+  };
+
+  render() {
+    const { filter, visible } = this.state;
+    const visibleContacts = this.getVisibleContacts();
+
+    return (
+      <section className={css.phonebook}>
+        <h1 className={css.title}>Your amazing phonebook</h1>
+        <Form onSubmit={this.formSubmitHandler} />
+
+        <h2 className={css.subtitle}>Contacts</h2>
+        {visible ? (
+          <>
+            <Filter
+              onFilterChange={this.changeFilter}
+              filterContacts={filter}
+            />
+            <ContactList
+              contactsList={visibleContacts}
+              onDeleteContact={this.deleteContact}
+            />
+          </>
+        ) : (
+          <p className={css.text}>You have no contacts</p>
+        )}
+      </section>
+    );
+  }
+}
+App.protoTypes = {
+  visible: PropTypes.bool.isRequired,
+};
+export default App;
---
+++ src/components/Filter/filter.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { nanoid } from 'nanoid';
+import css from './filter.module.css';
+
+const nameFilterId = nanoid();
+
+const Filter = ({ filter, onFilterChange }) => (
+  <>
+    <label className={css.label} htmlFor={nameFilterId}>
+      Find contacts by name
+    </label>
+    <input
+      className={css.input}
+      type="text"
+      value={filter}
+      id={nameFilterId}
+      onChange={onFilterChange}
+    />
+  </>
+);
+
+Filter.propTypes = {
+	filter:  PropTypes.string,
+	onFilterChange: PropTypes.func.isRequired,
+};
+
+export default Filter;
